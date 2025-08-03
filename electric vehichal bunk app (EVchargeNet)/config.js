a77704bc76baecd2c14825ed9449b71951e60e65
Jenish Dobariya <jenishdobariya108@gmail.com>
2025-08-03 16:31:40 +0530

    Add files via upload

---
+++ electric vehichal bunk app (EVchargeNet)/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBj1gk5gDsjZ1AHEOotdWAyM0KZoogfxhI",
+    authDomain: "ev-charge-bunk.firebaseapp.com",
+    projectId: "ev-charge-bunk",
+    storageBucket: "ev-charge-bunk.appspot.com",
+    messagingSenderId: "19879401003",
+    appId: "1:19879401003:web:2194e631aa24e347fa3c6e"
+};
